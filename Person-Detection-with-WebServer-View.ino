ecff58ae96da8d6ff96eee17db8a2a645e5df328
md-ahad-hossain <136985057+md-ahad-hossain@users.noreply.github.com>
2025-09-17 00:50:35 +0600

    Update Person-Detection-with-WebServer-View.ino

--- Person-Detection-with-WebServer-View.ino
+++ Person-Detection-with-WebServer-View.ino
@@ -11,7 +11,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
+==============================================================================*/
 #include <WiFi.h>
 #include <WiFiManager.h>
 
@@ -171,4 +171,5 @@ void loop() {
   vTaskDelay(100); // 100 ms delay to avoid watchdog
 
 }
+
 
