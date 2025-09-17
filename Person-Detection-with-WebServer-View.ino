661f97fb964e0d6573690e37d48cd5d7d4f9d4b8
md-ahad-hossain <136985057+md-ahad-hossain@users.noreply.github.com>
2025-09-17 10:43:45 +0600

    Update Person-Detection-with-WebServer-View.ino

--- Person-Detection-with-WebServer-View.ino
+++ Person-Detection-with-WebServer-View.ino
@@ -4,7 +4,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -171,5 +171,6 @@ void loop() {
   vTaskDelay(100); // 100 ms delay to avoid watchdog
 
 }
+
 
 
