8aa2f7550f7b8e31f004f5f056ada35b3a88117c
md-ahad-hossain <uzzolhossain1971@gmail.com>
2025-09-17 15:43:54 +0600

    Update model_settings.h

--- model_settings.h
+++ model_settings.h
@@ -16,20 +16,23 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_MODEL_SETTINGS_H_
 #define TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_MODEL_SETTINGS_H_
 
-// Keeping these as constant expressions allow us to allocate fixed-sized arrays
-// on the stack for our working memory.
-
-// All of these values are derived from the values used during model training,
-// if you change your model you'll need to update these constants.
+// Model input image dimensions
 constexpr int kNumCols = 96;
 constexpr int kNumRows = 96;
 constexpr int kNumChannels = 1;
 
+// Maximum image size
 constexpr int kMaxImageSize = kNumCols * kNumRows * kNumChannels;
 
+// Categories
 constexpr int kCategoryCount = 2;
 constexpr int kPersonIndex = 1;
 constexpr int kNotAPersonIndex = 0;
-extern const char* kCategoryLabels[kCategoryCount];
+
+// Category labels
+inline const char* kCategoryLabels[kCategoryCount] = {
+    "notperson",
+    "person",
+};
 
 #endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_MODEL_SETTINGS_H_
