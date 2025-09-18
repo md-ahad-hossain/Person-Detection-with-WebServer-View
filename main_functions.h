6e08d64885ea8125433713554f5d663ce7220b50
md-ahad-hossain <uzzolhossain1971@gmail.com>
2025-09-19 01:35:23 +0600

    Update main_functions.h

--- main_functions.h
+++ main_functions.h
@@ -21,14 +21,6 @@ limitations under the License.
 extern "C" {
 #endif
 
-// Initializes all data needed for the example. The name is important, and needs
-// to be setup() for Arduino compatibility.
-void setup();
-
-// Runs one iteration of data gathering and inference. This should be called
-// repeatedly from the application code. The name needs to be loop() for Arduino
-// compatibility.
-void loop();
 
 #ifdef __cplusplus
 }
