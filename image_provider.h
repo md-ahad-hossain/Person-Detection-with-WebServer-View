cefc9c82ce64dac679c3584e1a29db1afa002af9
md-ahad-hossain <uzzolhossain1971@gmail.com>
2025-09-18 23:59:58 +0600

    Update image_provider.h

--- image_provider.h
+++ image_provider.h
@@ -16,57 +16,24 @@ limitations under the License.
 #ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_IMAGE_PROVIDER_H_
 #define TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_IMAGE_PROVIDER_H_
 
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <Arduino.h>
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_spi_flash.h"
-#include "esp_system.h"
-#include "esp_timer.h"
-
-#include "app_camera_esp.h"
-#include "esp_camera.h"
-#include "model_settings.h"
-#include "esp_main.h"
-
 #include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/micro/micro_log.h"
 
-// Camera initialization function
-inline TfLiteStatus InitCamera() {
-    int ret = app_camera_init(); // low-level init
-    return (ret == 0) ? kTfLiteOk : kTfLiteError;
-}
+// This is an abstraction around an image source like a camera, and is
+// expected to place 8-bit quantized sample data into the tensor.
+//
+// The assumption is that this will be
+// called in a low duty-cycle fashion in a low-power application.  In these
+// cases, the imaging sensor need not be run in a streaming mode, but rather can
+// be idled in a relatively low-power mode between calls to GetImage().  The
+// assumption is that the overhead and time of bringing the low-power sensor out
+// of this standby mode is commensurate with the expected duty cycle of the
+// application.  The underlying sensor may actually be put into a streaming
+// configuration, but the tensor provided to GetImage should not be
+// overwritten by the driver code until the next call to GetImage();
+//
+// For real applications, you should
+// ensure there's a specialized implementation that accesses hardware APIs.
 
-// Function to capture image and convert RGB565 -> grayscale
-inline TfLiteStatus GetImage(int image_width, int image_height, int channels, int8_t* image_data) {
-    camera_fb_t* fb = esp_camera_fb_get();
-    if (!fb) return kTfLiteError;
-
-    int rows = (fb->height < kNumRows) ? fb->height : kNumRows;
-    int cols = (fb->width  < kNumCols) ? fb->width  : kNumCols;
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            uint16_t pixel = ((uint16_t*)fb->buf)[i * fb->width + j];
-
-            uint8_t hb = pixel & 0xFF;
-            uint8_t lb = pixel >> 8;
-            uint8_t r = (lb & 0x1F) << 3;
-            uint8_t g = ((hb & 0x07) << 5) | ((lb & 0xE0) >> 3);
-            uint8_t b = (hb & 0xF8);
-
-            int8_t grey = ((305 * r + 600 * g + 119 * b) >> 10) - 128;
-            image_data[i * kNumCols + j] = grey;
-        }
-    }
-
-    esp_camera_fb_return(fb);
-    return kTfLiteOk;
-}
+TfLiteStatus GetImage(const TfLiteTensor* tensor);
 
 #endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_IMAGE_PROVIDER_H_
